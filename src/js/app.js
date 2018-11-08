a8cb5555f3cc6f7373537255eaa3f7d9da241b85
Severin Kilchhofer <>
2018-11-08 19:22:28 +0100

    added fullscreen function added font awesome

--- src/js/app.js
+++ src/js/app.js
@@ -27,6 +27,19 @@ const state = {
 
 };
 
+    var elem = document.documentElement;
+    const openFullscreen = () => {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+        }
+    };
+
 const getMieteinnahmen = () => {
     return $('#mieteinnahmenCounter');
 };
